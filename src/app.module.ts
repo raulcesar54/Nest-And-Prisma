e5ba02164ef4801a090c205459cb083d827d27af
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-18 13:31:18 -0300

    criado mensagens iniciais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AddController } from './messages/add/add.controller';
 import { MessagesController } from './messages/messages.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, AddController, MessagesController],
+  controllers: [AppController, MessagesController],
   providers: [AppService],
 })
 export class AppModule {}
