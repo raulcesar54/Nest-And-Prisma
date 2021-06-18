d7a4b35d7c42811f7c8c2cc454cd85576dee2f45
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-18 13:22:28 -0300

    created new controle, by cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AddController } from './messages/add/add.controller';
+import { MessagesController } from './messages/messages.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AddController, MessagesController],
   providers: [AppService],
 })
 export class AppModule {}
